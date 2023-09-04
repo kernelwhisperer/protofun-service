98b1711e8e9f9e3b30582f5296a49878c4762880
Daniel Constantin <hello@danielconstantin.net>
2023-09-04 17:35:41 +0300

    Generate feathers app

--- src/app.ts
+++ src/app.ts
@@ -1,68 +1,54 @@
+// For more information about this file see https://dove.feathersjs.com/guides/cli/application.html
 import { feathers } from '@feathersjs/feathers'
+import configuration from '@feathersjs/configuration'
+import { koa, rest, bodyParser, errorHandler, parseAuthentication, cors, serveStatic } from '@feathersjs/koa'
+import socketio from '@feathersjs/socketio'
 
-// This is the interface for the message data
-interface Message {
-  id?: number
-  text: string
-}
+import { configurationValidator } from './configuration'
+import type { Application } from './declarations'
+import { logError } from './hooks/log-error'
+import { postgresql } from './postgresql'
+import { services } from './services/index'
+import { channels } from './channels'
 
-// A messages service that allows us to create new
-// and return all existing messages
-class MessageService {
-  messages: Message[] = []
+const app: Application = koa(feathers())
 
-  async find() {
-    // Just return all our messages
-    return this.messages
-  }
+// Load our app configuration (see config/ folder)
+app.configure(configuration(configurationValidator))
 
-  async create(data: Pick<Message, 'text'>) {
-    // The new message is the data text with a unique identifier added
-    // using the messages length since it changes whenever we add one
-    const message: Message = {
-      id: this.messages.length,
-      text: data.text
+// Set up Koa middleware
+app.use(cors())
+app.use(serveStatic(app.get('public')))
+app.use(errorHandler())
+app.use(parseAuthentication())
+app.use(bodyParser())
+
+// Configure services and transports
+app.configure(rest())
+app.configure(
+  socketio({
+    cors: {
+      origin: app.get('origins')
     }
+  })
+)
+app.configure(postgresql)
+app.configure(services)
+app.configure(channels)
 
-    // Add new message to the list
-    this.messages.push(message)
-
-    return message
-  }
-}
-
-// This tells TypeScript what services we are registering
-type ServiceTypes = {
-  messages: MessageService
-}
-
-const app = feathers<ServiceTypes>()
-
-// Register the message service on the Feathers application
-app.use('messages', new MessageService())
-
-// Log every time a new message has been created
-app.service('messages').on('created', (message: Message) => {
-  console.log('A new message has been created', message)
+// Register hooks that run on all service methods
+app.hooks({
+  around: {
+    all: [logError]
+  },
+  before: {},
+  after: {},
+  error: {}
+})
+// Register application setup and teardown hooks here
+app.hooks({
+  setup: [],
+  teardown: []
 })
 
-// A function that creates messages and then logs
-// all existing messages on the service
-const main = async () => {
-  // Create a new message on our message service
-  await app.service('messages').create({
-    text: 'Hello Feathers'
-  })
-
-  // And another one
-  await app.service('messages').create({
-    text: 'Hello again'
-  })
-
-  // Find all existing messages
-  const messages = await app.service('messages').find()
-
-  console.log('All messages', messages)
-}
-
-main()
+export { app }
