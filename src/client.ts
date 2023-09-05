0795f520627421ecb93402cbda5a3d6e92fe19d7
Daniel Constantin <hello@danielconstantin.net>
2023-09-05 16:12:57 +0300

    Add users service

--- src/client.ts
+++ src/client.ts
@@ -4,6 +4,9 @@ import type { TransportConnection, Application } from '@feathersjs/feathers'
 import authenticationClient from '@feathersjs/authentication-client'
 import type { AuthenticationClientOptions } from '@feathersjs/authentication-client'
 
+import { userClient } from './services/users/users.shared'
+export type { User, UserData, UserQuery, UserPatch } from './services/users/users.shared'
+
 import { alertsClient } from './services/alerts/alerts.shared'
 export type { Alerts, AlertsData, AlertsQuery, AlertsPatch } from './services/alerts/alerts.shared'
 
@@ -34,5 +37,6 @@ export const createClient = <Configuration = any,>(
   client.set('connection', connection)
 
   client.configure(alertsClient)
+  client.configure(userClient)
   return client
 }
